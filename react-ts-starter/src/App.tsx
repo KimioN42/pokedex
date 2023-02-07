9392327b6e2543ae92f9acf4effc33082e9968d7
Kimio Nishino <kimionishino1@gmail.com>
2023-02-06 21:00:56 -0500

    :bug: Fixed toasts taking too long in the screen

--- react-ts-starter/src/App.tsx
+++ react-ts-starter/src/App.tsx
@@ -8,11 +8,11 @@ function App() {
       <AppRouter />
       <ToastContainer
         position="top-center"
-        autoClose={5000}
+        autoClose={2000}
         hideProgressBar={false}
-        newestOnTop={false}
+        newestOnTop={true}
         closeOnClick
-        rtl={false}
+        rtl={true}
         pauseOnFocusLoss
         draggable
         pauseOnHover
